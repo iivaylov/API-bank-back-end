c1e2d0b76a0dc24cb63da3e73f7afe927ef2d2ac
Ivaylo Iliev <ilievivaylo200@proton.me>
2023-08-06 17:45:41 +0300

    add transfer tests

--- axe/src/test/java/io/axe/bank/Helpers.java
+++ axe/src/test/java/io/axe/bank/Helpers.java
@@ -1,7 +1,12 @@
 package io.axe.bank;
 
 import io.axe.bank.controllers.requests.RegisterRequest;
+import io.axe.bank.controllers.requests.TransferRequest;
+import io.axe.bank.models.Account;
 import io.axe.bank.models.User;
+import io.axe.bank.models.enums.AccountStatus;
+import io.axe.bank.models.enums.AccountType;
+import io.axe.bank.services.dtos.AccountDTO;
 import io.axe.bank.services.dtos.UserDTO;
 
 import java.time.LocalDateTime;
@@ -15,6 +20,17 @@ public class Helpers {
                 "Mccoy",
                 "user@example.com",
                 "1-562-388-7481");
+    }
+
+    public static AccountDTO createAccountDTO(){
+        return new AccountDTO(
+                1,
+                "AXE#U26VNZ1SBD0TPO1V",
+                AccountType.SVGS,
+                AccountStatus.ENABLED,
+                100.00,
+                "USD"
+        );
     }
 
     public static User createMockUser() {
@@ -33,6 +49,52 @@ public class Helpers {
         return mockUser;
     }
 
+    public static Account createMockAccount(){
+        Account mockAccount = new Account();
+        mockAccount.setId(1);
+        mockAccount.setIban("AXE#U26VNZ1SBD0TPO1V");
+        mockAccount.setAccountType(AccountType.SVGS);
+        mockAccount.setAccountStatus(AccountStatus.ENABLED);
+        mockAccount.setBalance(100.00);
+        mockAccount.setCurrency("USD");
+        mockAccount.setClosed(false);
+
+        return mockAccount;
+    }
+
+    public static AccountDTO createMockFromAccountDTO() {
+        return new AccountDTO(1,
+                "sender_account_iban",
+                AccountType.SVGS,
+                AccountStatus.ENABLED,
+                1000.00,
+                "USD");
+    }
+
+    public static Account createMockSenderAccount(){
+       Account senderAccount = new Account();
+       senderAccount.setId(1);
+       senderAccount.setIban("sender_account_iban");
+       senderAccount.setAccountType(AccountType.SVGS);
+       senderAccount.setAccountStatus(AccountStatus.ENABLED);
+       senderAccount.setBalance(1000.00);
+       senderAccount.setCurrency("USD");
+       senderAccount.setClosed(false);
+       return senderAccount;
+    }
+
+    public static Account createMockReceiverAccount(){
+        Account receiverAccount = new Account();
+        receiverAccount.setId(2);
+        receiverAccount.setIban("receiver_account_iban");
+        receiverAccount.setAccountType(AccountType.SVGS);
+        receiverAccount.setAccountStatus(AccountStatus.ENABLED);
+        receiverAccount.setBalance(500.00);
+        receiverAccount.setCurrency("USD");
+        receiverAccount.setClosed(false);
+        return receiverAccount;
+    }
+
     public static RegisterRequest registration() {
         RegisterRequest register = new RegisterRequest();
         register.setFirstname("Yetta");
@@ -44,5 +106,13 @@ public class Helpers {
         register.setTownName("Newport");
         register.setCountry("Russian Federation");
         return register;
+    }
+
+    public static TransferRequest transfer(){
+        TransferRequest transfer = new TransferRequest();
+        transfer.setAccountToIban("receiver_account_iban");
+        transfer.setAmountToTransfer(500.00);
+
+        return transfer;
     }
 }
--- axe/src/test/java/io/axe/bank/services/TransferServiceTests.java
+++ axe/src/test/java/io/axe/bank/services/TransferServiceTests.java
@@ -1,4 +1,51 @@
 package io.axe.bank.services;
 
-public class TransferServiceTests {
+import io.axe.bank.Helpers;
+import io.axe.bank.controllers.requests.TransferRequest;
+import io.axe.bank.models.Account;
+import io.axe.bank.repositories.AccountDAO;
+import io.axe.bank.services.dtos.AccountDTO;
+import io.axe.bank.services.impls.TransferServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+class TransferServiceTests {
+
+    @Mock
+    private AccountDAO mockRepositoryDAO;
+
+    @InjectMocks
+    private TransferServiceImpl service;
+
+    @Test
+    void testInnerTransferFunds() {
+        // Test data
+        AccountDTO fromAccountDTO = Helpers.createMockFromAccountDTO();
+        Account senderAccount = Helpers.createMockSenderAccount();
+        TransferRequest transferRequest = Helpers.transfer();
+        Account receiverAccount = Helpers.createMockReceiverAccount();
+
+        Mockito.when(mockRepositoryDAO.getAccountById(fromAccountDTO.id())).thenReturn(Optional.of(senderAccount));
+        Mockito.when(mockRepositoryDAO.getAccountByIban(transferRequest.getAccountToIban()))
+                .thenReturn(Optional.of(receiverAccount));
+
+        service.innerTransferFunds(transferRequest, fromAccountDTO);
+
+        Mockito.verify(mockRepositoryDAO).getAccountById(fromAccountDTO.id());
+        Mockito.verify(mockRepositoryDAO).getAccountByIban(transferRequest.getAccountToIban());
+        Mockito.verify(mockRepositoryDAO).updateAccount(senderAccount);
+        Mockito.verify(mockRepositoryDAO).updateAccount(receiverAccount);
+
+        double expectedSenderBalance = 500.0;
+        double expectedReceiverBalance = 1000.0;
+        assert senderAccount.getBalance() == expectedSenderBalance;
+        assert receiverAccount.getBalance() == expectedReceiverBalance;
+    }
 }
